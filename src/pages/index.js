b49db6e76ac98b94a44c6623332a120ae9f647e5
Vrymel <vrymel.dev@gmail.com>
2021-04-25 11:34:09 +0800

    Added branding

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,11 +20,11 @@ const IndexPage = () => {
     const is100k = price >= 100000;
 
     return (
-        <main>
+        <div className="h-screen flex flex-col">
             <title>Is BTC 100k Yet?</title>
 
-            <div className="h-screen flex flex-col justify-center items-center">
-                <div className="">
+            <main className="flex-1">
+                <div className="h-full flex flex-col justify-center items-center">
                     <h1 className="text-center text-3xl leading-8 font-bold tracking-tight text-gray-800 sm:text-4xl">
                         Is BTC 100k Yet?
 
@@ -33,11 +33,20 @@ const IndexPage = () => {
 
                     <div className="mt-6">
                         {price &&
-                        <h2 className="text-center text-6xl font-extrabold text-yellow-500">{numberFormat.format(price)}</h2>}
+                        <h2 className="text-center text-6xl font-extrabold text-yellow-500">
+                            <a href="https://www.coindesk.com/price/bitcoin">
+                                {numberFormat.format(price)}
+                            </a>
+                        </h2>}
                     </div>
                 </div>
-            </div>
-        </main>
+            </main>
+
+            <footer className="flex justify-between px-6 py-2">
+                <p className="text-gray-800">Made by <a href="https://vrymel.com" className="text-yellow-500 font-bold">Vrymel</a></p>
+                <p className="text-gray-800">Powered by <a href="https://www.coindesk.com/price/bitcoin" className="text-yellow-500 font-bold">CoinDesk</a></p>
+            </footer>
+        </div>
     )
 }
 
