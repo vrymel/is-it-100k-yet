41b1ff22eff882fdc0589a9cf8d86fe401cf2922
Vrymel <vrymel.dev@gmail.com>
2021-04-25 11:12:08 +0800

    Added basic styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,35 @@ const IndexPage = () => {
         fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
             .then(response => response.json())
             .then(data => {
-                setPrice(data.bpi?.USD?.rate);
+                let string = data.bpi?.USD?.rate || '';
+                string = string.replace(',', '');
+                let number = parseFloat(string).toFixed(2)
+                setPrice(number);
             })
             .catch(error => console.error(error))
     }, []);
 
+    const numberFormat = new Intl.NumberFormat();
+    const is100k = price >= 100000;
+
     return (
         <main>
-            <title>Is It 100k Yet?</title>
-            <h1 className="font-bold">Is It 100k Yet?</h1>
-            <p>{price}</p>
+            <title>Is BTC 100k Yet?</title>
+
+            <div className="h-screen flex flex-col justify-center items-center">
+                <div className="">
+                    <h1 className="text-center text-3xl leading-8 font-bold tracking-tight text-gray-800 sm:text-4xl">
+                        Is BTC 100k Yet?
+
+                        <span className="ml-2 font-black text-gray-900">{is100k ? 'Yes! 😱' : 'Not yet. 😴'}</span>
+                    </h1>
+
+                    <div className="mt-6">
+                        {price &&
+                        <h2 className="text-center text-6xl font-extrabold text-yellow-500">{numberFormat.format(price)}</h2>}
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
