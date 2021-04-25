1b7932279b82532d31ac12490c94eb57626de3b6
Vrymel <vrymel.dev@gmail.com>
2021-04-25 11:45:43 +0800

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  siteMetadata: {
-    title: "Is It 100k Yet?",
-  },
-  plugins: ['gatsby-plugin-postcss'],
+    siteMetadata: {
+        title: "Is It 100k Yet?",
+    },
+    plugins: [
+        'gatsby-plugin-postcss',
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Is BTC 100k yet?`,
+                short_name: `Is BTC 100k yet?`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#ff9140`,
+                display: `standalone`,
+                icon: `src/images/bitcoin.png`
+            },
+        }
+    ],
 };
